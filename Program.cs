47cd56fa8838bb69f2af7a02a48bfb581337c582
Taha <m.taha.ozhan@gmail.com>
2026-03-17 21:23:21 +0100

    Add error check for empty userName

--- Program.cs
+++ Program.cs
@@ -5,3 +5,12 @@ static void GreetUser(string name)
 {
     Console.WriteLine($"Hello, {name}!");
 }
+
+int userAge = 21;
+Console.WriteLine($"User age: {userAge}");
+
+string userName = "";
+if(string.IsNullOrEmpty(userName))
+{
+    Console.WriteLine("Error: userName is empty!");
+}
